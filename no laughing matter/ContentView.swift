ff5fef95de19cee7248b1be5ca70e10ddd1bfa2e
Florian <106911630+lflorian@users.noreply.github.com>
2025-09-14 15:56:27 +0200

    initial commit

--- no laughing matter/ContentView.swift
+++ no laughing matter/ContentView.swift
@@ -8,17 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var viewModel = ViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
     }
 }
 
-#Preview {
-    ContentView()
-}
+#Preview { ContentView() }
---
+++ no laughing matter/ContentViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ContentViewModel.swift
+//  no laughing matter
+//
+//  Created by Florian Lammert on 14.09.25.
+//
+
+import Foundation
+
+extension ContentView {
+    @Observable
+    class ViewModel {
+        
+    }
+}
