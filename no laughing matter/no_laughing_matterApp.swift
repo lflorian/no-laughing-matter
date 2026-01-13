a3cf8fb66ca6888d1def19e77c356c1202d3bc44
Florian <106911630+lflorian@users.noreply.github.com>
2026-01-13 14:44:40 +0100

    Initial Commit

---
+++ no laughing matter/no_laughing_matterApp.swift
@@ -0,0 +1,17 @@
+//
+//  no_laughing_matterApp.swift
+//  no laughing matter
+//
+//  Created by Florian Lammert on 13.01.26.
+//
+
+import SwiftUI
+
+@main
+struct no_laughing_matterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
